45dfac7e597b9fb509a64c3c0d7b64469e2aa3f5
Yousef Mahmoud Ali <YousefMahmoudAli@outlook.com>
2026-07-18 20:32:25 +0300

    fix: make base path dynamic for Vercel and GitHub Pages compatibility

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/IEEENUWebSiteFrontEnd/',
+  base: process.env.GH_PAGES ? '/IEEENUWebSiteFrontEnd/' : '/',
   server: {
     port: 3000
   }
 });
+
 
